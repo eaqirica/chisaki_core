eb63af9bc2670ad30a284f5611b9ab6611d6b243
thelazypie <sven49376@gmail.com>
2019-08-11 18:00:53 +0300

    test command

--- src/commands/fun/test.ts
+++ src/commands/fun/test.ts
@@ -1,9 +1,10 @@
-import Command from "@type/Command";
+import { Message } from "discord.js";
+import Command from '@type/Command'
 
 const test: Command = {
     name: "test",
     type: "SYSTEM",
-    summon: (args: string) => console.log('hello world')
+    summon: (message: Message, args: string[]) => console.log('hello world')
 }
 
 export default test;
