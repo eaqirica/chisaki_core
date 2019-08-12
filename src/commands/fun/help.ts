488749de9ec25d17c9d187bf0a4274175348d66b
thelazypie <sven49376@gmail.com>
2019-08-13 00:11:28 +0300

    help command change

--- src/commands/fun/help.ts
+++ src/commands/fun/help.ts
@@ -1,4 +1,4 @@
-import { Message } from "discord.js";
+import { Message, RichEmbed } from "discord.js";
 import Command from '@type/Command'
 import { ExtendedClient } from "ExtendedClient";
 
@@ -9,11 +9,24 @@ const help: Command = {
     summon: async (exClient: ExtendedClient, message: Message, args: string[]) => {
         //request to db and increase warn count
         await message.delete();
-        message.channel.send('<-=-=-=-=-=-=-=-=-=-=-!-=-=-=-=-=-=-=-=-=-=->');
+
+        await message.channel.send('```<-=-=-=-=-=-=-=-=-=-=-!-=-=-=-=-=-=-=-=-=-=->```');
+
+        const RichCommand = new RichEmbed();
+        RichCommand.setTitle("Commands...");
+        RichCommand.setURL('https://ryuunosuke.space/commands');
+        RichCommand.setColor("#30ba8f");
+        RichCommand.setTimestamp(new Date());
+        RichCommand.setFooter(message.author.username, message.author.avatarURL);
+
         exClient.commands.forEach((command: Command) => {
-            message.channel.send(`type = ${command.type} | name = ${command.name} | usage = ${command.help}`)
+            // message.channel.send(`type = ${command.type} | name = ${command.name} | usage = ${command.help}`);
+            RichCommand.addField(`Name: ${command.name}`, `Usage: <${command.help}>`);
+            RichCommand.addBlankField();
         })
-        message.channel.send('<-=-=-=-=-=-=-=-=-=-=-¡-=-=-=-=-=-=-=-=-=-=->');
+
+        await message.channel.send(RichCommand);
+        await message.channel.send('```<-=-=-=-=-=-=-=-=-=-=-¡-=-=-=-=-=-=-=-=-=-=->```');
     }
 }
 
