b0d3bd0bf80f18b5e6a8b31051c101c8b683e726
thelazypie <sven49376@gmail.com>
2019-08-17 01:04:24 +0300

    fix: bigtext

--- src/commands/fun/bigText.ts
+++ src/commands/fun/bigText.ts
@@ -1,6 +1,7 @@
 import { Message, RichEmbed } from "discord.js";
 import Command from '@type/Command'
 import { ExtendedClient } from "ExtendedClient";
+import { isAlpha } from 'validator'
 
 const help: Command = {
     name: "bigtext",
@@ -8,10 +9,11 @@ const help: Command = {
     type: "FUN",
     summon: async (exClient: ExtendedClient, message: Message, args: string[]) => {
 
-        //make it better
-        //problem: unhandled unexpected chars
-        const letters: string[] = args.toString().toLowerCase().replace(",", "").split('').map((letter: string) => `:regional_indicator_${letter}:`);
-        console.log(letters)
+        const preparedString = args.toString().toLowerCase().replace(/,/g, "").trim();
+
+        if (!isAlpha(preparedString)) return await message.channel.send('NANI?')
+
+        const letters: string[] = preparedString.split('').map((letter: string) => `:regional_indicator_${letter}:`);
 
         await message.channel.send(letters.join(''));
     }
