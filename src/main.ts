eb543d665702118159bb0edc93417dc3c1bdd075
thelazypie <sven49376@gmail.com>
2019-08-17 01:05:36 +0300

    nani?

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ dotenv.config();
 
 import { ExtendedClient } from "./ExtendedClient";
 
+const chisaki = new ExtendedClient();
 (async () => {
-    const chisaki = new ExtendedClient();
     //todo: await chisaki.enableLogging(loggingType);
     await chisaki.loadCommands('build/commands');
     await chisaki.login(process.env.BOT_TOKEN);
-})()
+})().then(async () => {
+    //const owner = await chisaki.fetchUser('236442313918775296');
+    await chisaki.user.setPresence({ status: 'dnd', game: { name: "твой шёпот", type: "LISTENING" } })
+})
