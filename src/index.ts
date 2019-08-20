e1165e97c0fb62be9720deb024c94618ff8b106f
thelazypie <sven49376@gmail.com>
2019-08-21 01:57:39 +0300

    main

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+export { ExtendedCLient } from './models/ExtendedClient';
+export { Module } from './models/Module';
+export { ModuleData } from './models/ModuleData';
+export { ModuleLoader } from './models/ModuleLoader';
